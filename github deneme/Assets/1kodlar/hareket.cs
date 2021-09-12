6c53261d8c824846f47e3eb142a887229d3e2e10
nasmos <nasmosia@gmail.com>
2021-09-12 08:50:23 +0300

    Revert "Revert "kod ekledim""
    This reverts commit 3564d38f0bd79bc1dd525ea4ed5a2ffa90d73c42.

---
+++ github deneme/Assets/1kodlar/hareket.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hareket : MonoBehaviour
+{
+    public float hiz;
+    public float ziplamahiz;
+
+    private Rigidbody2D rb;
+
+    public Transform yer; //, yer2;
+    public LayerMask yerne;
+    private bool yerdemi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * hiz, rb.velocity.y);
+
+        yerdemi = Physics2D.OverlapCircle(yer.position, .1f, yerne); //|| Physics2D.OverlapCircle()
+
+        
+
+        if (Input.GetButtonDown("Jump") && yerdemi)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, ziplamahiz);
+        }
+
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y *0.5f);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D coll)
+
+    {  
+
+        if (coll.gameObject.tag == "zipzip")
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2 (20,7)   ;
+        }
+    }
+}
+
